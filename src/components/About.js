38ca0d6c75ea865cc83dd02ee9bd223adf629925
AlexIrungu <aleaxmuiruri@gmail.com>
2023-08-03 12:30:40 +0300

    last updates

--- src/components/About.js
+++ src/components/About.js
@@ -7,7 +7,7 @@ function About() {
   const [isOpen, setOpen] = useState(false);
 
   return (
-    <div className="bg-gray-100 py-12">
+    <div className="bg-green-100 py-12">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="lg:text-center">
           <div className="bg-white rounded-lg shadow-lg p-8">
