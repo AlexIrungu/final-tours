9f7fd77777e883f4bb65db0a95bfe9f295f7b5f7
AlexIrungu <aleaxmuiruri@gmail.com>
2023-08-05 14:28:14 +0300

    commented out a <p>

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -23,7 +23,7 @@ function LandingPage() {
             <h1 className="text-4xl font-bold mb-4">
               <span className="text-white font-bold">Welcome to Danil Scenic Tours</span>
             </h1>
-            <p className="text-l mb-6 font-bold">Explore the scenic beauty vibrant culture, &                  abundant wildlife of the    region.</p>
+            {/* <p className="text-l mb-6 font-bold">Explore the scenic beauty vibrant culture, &                  abundant wildlife of the    region.</p> */}
             <p className="text-l mb-6 font-bold">Click Here to Discover.</p>
             <Link
   to="/home"
