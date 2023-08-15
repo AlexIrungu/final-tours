9ce33e2c7ea187b1440e440ec3c23e96e7e82b2b
AlexIrungu <aleaxmuiruri@gmail.com>
2023-08-15 09:36:34 +0300

    maintanence

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,40 +1,40 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import { LightSpeed } from "react-reveal"; // Import the LightSpeed animation from react-reveal
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faArrowDown } from "@fortawesome/free-solid-svg-icons"; // Import the arrow down icon
+// import React from "react";
+// import { Link } from "react-router-dom";
+// import { LightSpeed } from "react-reveal"; // Import the LightSpeed animation from react-reveal
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// import { faArrowDown } from "@fortawesome/free-solid-svg-icons"; // Import the arrow down icon
 
-import flam from './assets/flammm.jpg'
+// import flam from './assets/flammm.jpg'
 
-function LandingPage() {
-  return (
-    <div className="landing-page h-screen flex flex-col">
-      <div className="hero-image-container relative flex-1">
-        <img
-          src={flam}
-          alt="Beautiful Destination"
-          className="w-full h-full object-cover"
-        />
+// function LandingPage() {
+//   return (
+//     <div className="landing-page h-screen flex flex-col">
+//       <div className="hero-image-container relative flex-1">
+//         <img
+//           src={flam}
+//           alt="Beautiful Destination"
+//           className="w-full h-full object-cover"
+//         />
 
-        <div className="absolute top-0 left-0 w-full h-full bg-black bg-opacity-50"></div>
+//         <div className="absolute top-0 left-0 w-full h-full bg-black bg-opacity-50"></div>
 
-        <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-customWhite text-center ">
-          <LightSpeed cascade>
-            <h1 className="text-4xl font-bold mb-4 ">
-              <span className="text-customWhite font-bold ">Welcome to Danil Scenic Tours</span>
-            </h1>
-            <p className="text-l mb-6 font-bold">Click Here <FontAwesomeIcon icon={faArrowDown} /></p>
-            <Link
-              to="/home"
-              className="px-8 py-3 rounded-full bg-green-600 hover:bg-green-600 text-white font-bold"
-            >
-              Discover Now
-            </Link>
-          </LightSpeed>
-        </div>
-      </div>
-    </div>
-  );
-}
+//         <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-customWhite text-center ">
+//           <LightSpeed cascade>
+//             <h1 className="text-4xl font-bold mb-4 ">
+//               <span className="text-customWhite font-bold ">Welcome to Danil Scenic Tours</span>
+//             </h1>
+//             <p className="text-l mb-6 font-bold">Click Here <FontAwesomeIcon icon={faArrowDown} /></p>
+//             <Link
+//               to="/home"
+//               className="px-8 py-3 rounded-full bg-green-600 hover:bg-green-600 text-white font-bold"
+//             >
+//               Discover Now
+//             </Link>
+//           </LightSpeed>
+//         </div>
+//       </div>
+//     </div>
+//   );
+// }
 
-export default LandingPage;
+// export default LandingPage;
