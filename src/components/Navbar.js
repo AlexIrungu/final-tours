834b4ce8ec925a4d816c9360852664b3a101edf7
AlexIrungu <aleaxmuiruri@gmail.com>
2023-12-10 19:19:12 +0300

    updates

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,114 +5,78 @@ import './Navbar.css'
 
 const Navbar = () => {
   const [isMenuOpen, setMenuOpen] = useState(false);
-  const [isPlacesDropdownOpen, setPlacesDropdownOpen] = useState(false);
+  const [isDropdownOpen, setDropdownOpen] = useState(false);
   const history = useHistory();
+
+  const toggleDropdown = () => {
+    setDropdownOpen((prevDropdownState) => !prevDropdownState);
+  };
 
   const toggleMenu = () => {
     setMenuOpen((prevMenuState) => !prevMenuState);
   };
 
-  const togglePlacesDropdown = () => {
-    setPlacesDropdownOpen((prevDropdownState) => !prevDropdownState);
-  };
-
   const handleDropdownItemClick = (route) => {
-    setPlacesDropdownOpen(false);
+    setDropdownOpen(false);
     history.push(route);
   };
 
   return (
     <nav className="bg-brown-600 text-dark p-4">
       <div className="container mx-auto flex items-center justify-between">
-        
         <Link to="/" className="logo-link">
-          <img src={logo} alt="Logo" className="object-cover w-30 h-20" />
+          <img src={logo} alt="Logo" className="logo-image" />
         </Link>
-      
         <button
           className={`menu-button show-menu-button lg:hidden`}
           onClick={toggleMenu}
         >
           <svg
-            className={`w-6 h-6 fill-current ${isMenuOpen ? "text-customWhite" : "text-black"}`}
+            className={`w-6 h-6 fill-current ${
+              isMenuOpen ? "text-customWhite" : "text-black"
+            }`}
             viewBox="0 0 24 24"
             xmlns="http://www.w3.org/2000/svg"
           >
-            {/* SVG path */}
+            <path
+              d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8zm-1-14h2v6h-2zm0 8h2v2h-2z"
+            />
           </svg>
         </button>
         <div
-          className={`menu lg:flex ${isMenuOpen ? "block" : "show"} transition-all duration-500`}
+          className={`menu lg:flex ${isMenuOpen ? "block" : "hidden"} transition-all duration-500`}
         >
-          <ul className="menu-list flex space-x-4 lg:space-x-8 font-bold">
+          <ul className="menu-list flex flex-col space-y-4 lg:space-x-4 lg:space-y-0 lg:flex-row lg:items-center">
             <li className="menu-item">
-              <Link to="/" className="menu-link hover:text-green-600">
+              <Link to="/" className="menu-link hover:text-green-600 px-4 py-2 font-bold">
                 Home
               </Link>
             </li>
             <li className="menu-item">
-              <Link to="/about" className="menu-link hover:text-green-600">
+              <Link to="/about" className="menu-link hover:text-green-600 px-4 py-2 font-bold">
                 About
               </Link>
             </li>
-            {/* <li className="menu-item">
-              <Link to="/service" className="menu-link hover:text-green-600">
-                Service
-              </Link>
-            </li> */}
             <li className="menu-item">
-              <Link to="/places">
-              <button className="menu-link hover:text-green-600" onClick={togglePlacesDropdown}>
+              <Link to="/places" className="menu-link hover:text-green-600 px-4 py-2 font-bold">
                 Places
-              </button>
               </Link>
-              {isPlacesDropdownOpen && (
-                <ul className="places-dropdown">
-                  {/* <li className="dropdown-item">
-                    <button
-                      className="dropdown-link hover:text-green-600"
-                      onClick={() => handleDropdownItemClick("/service")}
-                    >
-                      Service
-                    </button>
-                  </li> */}
-                  {/* <li className="dropdown-item">
-                    <button
-                      className="dropdown-link hover:text-green-600"
-                      onClick={() => handleDropdownItemClick("/section")}
-                    >
-                      Service
-                    </button>
-                  </li> */}
-                  {/* Add more dropdown items for other places */}
-                </ul>
-              )}
             </li>
             <li className="menu-item">
-              <Link to="/gallery" className="menu-link hover:text-green-600">
+              <Link to="/gallery" className="menu-link hover:text-green-600 px-4 py-2 font-bold">
                 Gallery
               </Link>
             </li>
             <li className="menu-item">
-              <Link to="/maps" className="menu-link hover:text-green-600">
+              <Link to="/maps" className="menu-link hover:text-green-600 px-4 py-2 font-bold">
                 Map
               </Link>
             </li>
-            {/* <li className="menu-item">
-              <Link to="/faq" className="menu-link hover:text-green-600">
-                FAQ
-              </Link>
-            </li> */}
             <li className="menu-item">
-              <Link to="/contact" className="menu-link hover:text-green-600">
+              <Link to="/contact" className="menu-link hover:text-green-600 px-4 py-2 font-bold">
                 Contact Us
               </Link>
             </li>
-            {/* <li>
-              <Link to="/pricing" className="menu-link hover:text-green-600">
-                Pricing
-              </Link>
-            </li> */}
           </ul>
         </div>
       </div>
